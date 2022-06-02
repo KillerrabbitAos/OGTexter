561b1d6c361a5f8878787e9fba0ee24ee60e8535
Centiljard <89267105+Centiljard@users.noreply.github.com>
2022-06-02 15:31:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+import random
+import time
+import re
+import tools
+
+player_X = 0
+player_Y = 0
+
+run = True
+run2 = True
+run3 = True
+admin = False
+
+# persons
+player = tools.Person("steve", 12, "school", 100, ["steve", "Alex"], [], "unknown", "doing nothing")
+Steve = tools.Person("steve", 12, "home", 100, ["Alex", player.name], [], "unknown", "doing nothing")
+Alex = tools.Person("steve", 12, "school", 100, ["Steve", player.name], [],
+                    "unknown", "doing nothing")
+
+# locations
+school = tools.Location(True, True)
+
+# indicators
+indicator = "/"
+
+health_indicator_bar = (tools.convert_to_string(
+    ([indicator] * (int(player.health / 10)))))
+
+# main loops
+while run:
+
+    while run2:
+        print("It´s time to make your character.")
+        player.name = input("Character Name: ")
+        current_scene = 0
+        while run3:
+            choice = (random.randint(0, 100))
+            current_scene = current_scene + 1
+            current_conversation = False
+            player.action = "doing nothing"
+            hurt = 0
+            heal = 0
+            health_indicator_bar = (tools.convert_to_string(
+                ([indicator] * (int(player.health / 10)))))
+
+            print(
+                f"health:{health_indicator_bar} friends:{len(player.friends)}")
+
+            command = input("Write here: ")
+
+            if command == "__admin__":
+                subCommand = input("print OK after the semicolon to confirm, if not, just click enter;")
+                if subCommand == "OK":
+                    admin = True
+                    print("you are now __admin__")
+
+            if choice:
+                Steve.location = player.location
+            if command.startswith("walk to"):
+                player.location = (command.split()[2])
+                player.action = f"walking to {player.location}"
+                print(f"You walked to {player.location}")
+
+            if Steve.location == player.location:
+                current_conversation = tools.Conversation([Steve])
+
+
+            if command == "health":
+                print(f"Your health is {player.health}.")
+            if command == "location":
+                print(f"Location: {player.location}")
+            if command == "friends":
+                print(player.friends)
+            if command == "items":
+                print(player.items)
+            if command == "admin?":
+                if admin:
+                    print("yes")
+                else:
+                    print("no")
+
+            tools.scene(current_scene, player.action, player.name)
+
+
+            if admin:
+                if command.startswith("hurt"):
+                    hurt = re.findall("\d", command)
+                    hurt = tools.convert_to_string(hurt)
+                    print(hurt)
+                if command.startswith("heal") and command != "health":
+                    heal = re.findall("\d", command)
+                    heal = tools.convert_to_string(heal)
+                    print(heal)
+
+            player.health = player.health - int(hurt)
+            player.health = player.health + int(heal)
+            if player.location == "School":
+                school.Player_Presence = True
+
+
+
+
---
+++ tools.py
@@ -0,0 +1,43 @@
+def convert_to_string(text):
+    bar = ""
+    return bar.join(text)
+
+
+def write(text):
+    return f"test: {text}"
+
+
+class Location:
+    def __init__(self, Player_Presence, Possible_Destination):
+        self.Player_Presence = Player_Presence
+        self.Possible_Destination = Possible_Destination
+
+
+
+class Person:
+    def __init__(self, name, age, location, health, friends, items, home, action):
+        self.name = name
+        self.age = age
+        self.location = location
+        self.health = health
+        self.friends = friends
+        self.items = items
+        self.home = home
+        self.action = action
+
+
+class Conversation:
+    def __init__(self, members):
+        self.members = members
+
+
+def scene(number, action, player_name):
+    if number == 1:
+        print(f"Whilst {action} you stumble upon Mr.Z")
+        current_conversation = True
+        while current_conversation:
+            subcommand = input("What will you say?: ")
+            print(f"{subcommand} to you too {player_name}")
+            if subcommand == "hello" or subcommand == "Hello":
+                current_conversation = False
+
