2233467a1457d85b23f38a1f9895e63c5b9d360c
Artur Norrby <artur.norrby@gmail.com>
2022-06-06 14:36:49 +0200

    added totalTime parameter to the tools.draw function, the totalTime was also added to the "Welcome to Texter {player.name}!" animation

--- main.py
+++ main.py
@@ -37,8 +37,7 @@ while run:
         print("It´s time to make your character.")
         player.name = input("Character Name: ")
         current_scene = 0
-        tools.draw(f"Welcome to Texter {player.name}!")
-        time.sleep(1)
+        tools.draw(f"Welcome to Texter {player.name}!", 5, 1)
         while run3:
             choice = (random.randint(0, 100))
             current_scene = current_scene + 1
